d412b7b7981de3461ea7730d645f5d878c350813
Adam Demaree <demareeadam1@gmail.com>
2017-06-12 17:59:17 -0400

    initial commit and classes added

---
+++ Engine/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Player
+    {
+        //What is going on? :
+
+        //Property Names for Players
+        //Public = Scope of the class. Means visible throughout the whole solution.
+
+       //"int" is the data type. Int stands for integer, which deals only with whole numbers.
+       //"get" is a function that is getting a value to store in that property
+       //"set" is a function that is placing that value into the property
+
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+
+    }
+}
---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        //Scope: Private since we don't need anything outside of theis screen to use the variable
+        //Datatype: Player because we want to store a player object in it
+        //Name: _player because it is a private variable, which is just a normal naming convention to use "_" 
+        //when dealing with private variables.
+        //**Note: YOU MUST CONNECT THE "Player" CLASS WITH A USING STATEMENT ON TOP**
+
+        private Player _player;
+
+        //Constructor to the "SuperAdventure" class
+        //The code in the constructor is always public and is always named after the class, which is this case
+        //is the form
+
+        public SuperAdventure()
+        {     
+            InitializeComponent();
+
+            //This is where we will instantiate a "Player" object to store in the "_player" variable
+
+            _player = new Player();
+
+            //This ^^ creates a new player object using the "new Player();" code and assigning it 
+            //to the "_player" variable.
+
+            //Here we are assigning values to the properties of the "_player" object
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            //Here we are getting the value of the properties from the "_player" object, and assigning them to the 
+            //text of the labels on the screen
+            //**NOTE: THE "ToString()" METHOD ALLOWS INTEGER VALUES TO BE ASSIGNED TO A STRING**
+
+            label1.Text = _player.CurrentHitPoints.ToString();
+            label2.Text = _player.Gold.ToString();
+            label3.Text = _player.ExperiencePoints.ToString();
+            label4.Text = _player.Level.ToString();
+        }
+
+     
+    }
+}
